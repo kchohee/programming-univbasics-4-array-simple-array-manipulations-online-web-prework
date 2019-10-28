4f714e99cbb1717fe55e2b1c2ff6c0231fa3c386
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-10-28 02:59:08 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,63 @@
+#1 
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow.push(next_color)
+end
+
+#2
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
+bouroughs_in_nyc.unshift(new_neighborhood)
+return bouroughs_in_nyc
+end
+
+#3
+def using_pop(continents)
+continents.pop
+end
+
+#4
+def pop_with_args(dog_breeds)
+dog_breeds.pop
+dog_breeds.pop
+return dog_breeds
+end
+
+#5
+#def using_shift(my_favorite_cities)
+ #
+#end
+
+#6
+def shift_with_args(array)
+array.shift(2)
+
+end
+
+#7
+#def using_concat(my_favorite_things, more_favs)
+#
+#end
+
+#8
+#def using_insert(list_of_programming_languages, another_language)
+#
+#end
+
+#9
+#def using_uniq(haircuts)
+#
+#end
+
+#10
+#def using_flatten(instruments)
+#
+#end
+
+#11
+#def using_delete (instructors, "Steven")
+#
+#end
+
+#12
+#def using_delete_at(famous_robots, 2)
+#
+#end
