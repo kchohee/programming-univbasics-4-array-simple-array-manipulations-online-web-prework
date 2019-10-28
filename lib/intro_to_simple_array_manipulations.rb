0f5c6aabbc9ceab3aeb4842f104ac2aeeeef2f9c
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-10-28 03:27:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,19 +19,17 @@ def pop_with_args(array)
 array.pop(2)
 return array
 array.last
-array.push("Shiba Inu")
-return array
 end
 
 #5
-def using_shift(my_favorite_cities)
-
-end
+#def using_shift(my_favorite_cities)
+#
+#end
 
 #6
 def shift_with_args(array)
 array.shift(2)
-
+return array
 end
 
 #7
